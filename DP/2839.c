ddc8c56140f16266dad6ce4ce06a3d6b69836ea0
Choieeh <chyh0182@naver.com>
2020-11-25 14:57:25 +0000

    2839

---
+++ DP/2839.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    int res[1001][1700];
+    int res[0][0] = 0;
+    int i, j;
+    for(i=0; i<1001; i++) {
+        for(j=0; j<1700; j++) {
+            if()
+            res[i][j] = res[]
+        }
+    }
+}
---
+++ DP/2839.py
@@ -0,0 +1,7 @@
+N = input()
+
+
+
+if i in range(0, 10000):
+    if j in range(0, 1700):
+        
