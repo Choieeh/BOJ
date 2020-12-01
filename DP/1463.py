25c98a7d8e7035092b01fbb5e0ffcded9f18ce45
Choieeh <chyh0182@naver.com>
2020-12-01 14:57:11 +0000

    1463 clear with help

--- DP/1463.py
+++ DP/1463.py
@@ -1,21 +1,20 @@
 N = int(input())
 
-answer = 0
-count = 0
-if N == 1:
-    answer = 0
-else:
-    while(N != 1):
-        if N % 3 == 0:
-            N = N // 3
-            count=count+1
-        elif N % 2 == 0:
-            N = N // 2
-            count=count+1
-        else:
-            N = N - 1
-            count=count+1
+arr = [0 for i in range(N+1)]
 
-answer = count
-print(answer)
+arr[1] = 0
 
+for i in range(2, N+1):
+    arr[i] = arr[i-1] + 1
+    if(i % 2 == 0):
+        arr[i] = min(arr[i], arr[i//2]+1)
+    if(i % 3 == 0):
+        arr[i] = min(arr[i], arr[i//3]+1)
+        
+print(arr[N])
+
+#DP 할때는 이전식을 구할수 있는지 부터 확인!
+#여기서는 arr를 통해 1번부터 N까지 차례로 구해나감
+
+
+#https://stonejjun.tistory.com/24
