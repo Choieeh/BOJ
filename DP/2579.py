092358ac951ff07cf85755ddfdffb6f53f495fce
Choieeh <chyh0182@naver.com>
2020-12-03 14:45:14 +0000

    2579 clear

--- DP/2579.py
+++ DP/2579.py
@@ -1,8 +1,29 @@
 n = int(input())
 
+score = [0 for i in range(n+1)]
 stair = [0 for i in range(n+1)]
 for i in range(1, n+1):
     stair[i] = int(input())
 
 count = 0
-#count++해서 3이면 이단으로 건너뛰기
+
+score[0] = 0
+score[1] = stair[1]
+if n>=2 :
+    score[2] = stair[1]+stair[2]
+
+if n > 2:
+    for i in range(3, n+1):
+        score[i] = max(score[i-3]+stair[i-1]+stair[i], score[i-2]+stair[i])
+
+print(score[n])
+
+
+'''
+n번째면 n-1 && count = 1
+count = 0
+count = 1
+   or  n-2 count++
+   ooooo
+       
+'''
