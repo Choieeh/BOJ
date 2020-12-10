52cc81c9ba41ec21e8ff536474998fd972251d80
Choieeh <chyh0182@naver.com>
2020-12-10 14:51:33 +0000

    15486 solving

---
+++ DP/15486.py
@@ -0,0 +1,29 @@
+N = int(input())
+
+A = [[0 for x in range(2)] for y in range(N+1)]
+E = [0 for i in range(N+1)]
+S = [0 for i in range(N+1)]
+day = 1
+
+for i in range(1, N+1):
+    A[i][0], A[i][1] = input().split()
+    A[i][0] = int(A[i][0])
+    A[i][1] = int(A[i][1])
+    E[i] = i + A[i][0] - 1
+
+for i in range(1, N+1):
+    for j in range(1, i):
+        if E[j] == i:
+            if A[j][1] + S[i-1] > S[i]:
+                S[i] = A[j][1] + S[i-1]
+    
+
+print(S[N])
+            
+'''
+1 2 3 4 5 6 7
+3 5 1 A 2 4 2
+0 0 0 0 15 40 200
+3 6 3 4 6 9 8
+0 0 10 30 30 
+'''
