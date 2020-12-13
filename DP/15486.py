49e90b4e854c8babff2d3351eecbb30c45502ed9
Choieeh <chyh0182@naver.com>
2020-12-13 14:32:00 +0000

    15486 solving with help

--- DP/15486.py
+++ DP/15486.py
@@ -1,24 +1,20 @@
 N = int(input())
 
 A = [[0 for x in range(2)] for y in range(N+1)]
-E = [0 for i in range(N+1)]
-S = [0 for i in range(N+1)]
-day = 1
+S = [0 for i in range(N+2)]
 
 for i in range(1, N+1):
     A[i][0], A[i][1] = input().split()
     A[i][0] = int(A[i][0])
     A[i][1] = int(A[i][1])
-    E[i] = i + A[i][0] - 1
 
-for i in range(1, N+1):
-    for j in range(1, i):
-        if E[j] == i:
-            if A[j][1] + S[i-1] > S[i]:
-                S[i] = A[j][1] + S[i-1]
-    
+for i in range(1, N+2):
+    if i + A[i][0] <= N+1:
+        S[i+A[i][0]] = max(S[i+A[i][0]], S[i] + A[i][1])
+    if i < N:
+        S[i+1] = max(S[i], S[i+1])
 
-print(S[N])
+print(S[N+1])
             
 '''
 1 2 3 4 5 6 7
