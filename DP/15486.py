cc6f893a4d42ee250cd254c5d5dc575d972c506d
Choieeh <chyh0182@naver.com>
2020-12-14 14:06:14 +0000

    15486 clear with help

--- DP/15486.py
+++ DP/15486.py
@@ -1,25 +1,23 @@
+import sys
+
 N = int(input())
 
 A = [[0 for x in range(2)] for y in range(N+1)]
 S = [0 for i in range(N+2)]
 
 for i in range(1, N+1):
-    A[i][0], A[i][1] = input().split()
+    A[i][0], A[i][1] = sys.stdin.readline().split()
+    #input은 속도가 느림.
     A[i][0] = int(A[i][0])
     A[i][1] = int(A[i][1])
 
-for i in range(1, N+2):
+for i in range(1, N+1):
     if i + A[i][0] <= N+1:
         S[i+A[i][0]] = max(S[i+A[i][0]], S[i] + A[i][1])
-    if i < N:
-        S[i+1] = max(S[i], S[i+1])
+    S[i+1] = max(S[i], S[i+1])
 
 print(S[N+1])
             
 '''
-1 2 3 4 5 6 7
-3 5 1 A 2 4 2
-0 0 0 0 15 40 200
-3 6 3 4 6 9 8
-0 0 10 30 30 
+입출력 속도가 sys.stdin.readline() > raw_input() > input() 순으로 빨라서 input을 사용하면 시간초과가 되는것.
 '''
