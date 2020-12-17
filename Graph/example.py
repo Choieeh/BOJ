409d0820a0f871e59b78b222e770aed767e2080a
Choieeh <chyh0182@naver.com>
2020-12-17 14:24:12 +0000

    dfs bfs example

---
+++ Graph/example.py
@@ -0,0 +1,42 @@
+graph = {
+    'A': ['B'],
+    'B': ['A', 'C', 'H'],
+    'C': ['B', 'D'],
+    'D': ['C', 'E', 'G'],
+    'E': ['D', 'F'],
+    'F': ['E'],
+    'G': ['D'],
+    'H': ['B', 'I', 'J', 'M'],
+    'I': ['H'],
+    'J': ['H', 'K'],
+    'K': ['J', 'L'],
+    'L': ['K'],
+    'M': ['H']
+}
+
+
+def bfs(graph, start_node):
+    visit = list()
+    queue = list()
+    queue.append(start_node)
+    while queue:
+        node = queue.pop(0)
+        if node not in visit:
+            visit.append(node)
+            queue.extend(graph[node])
+            #extend는 list의 끝에 모든 항목을 덧붙여서 확장
+    return visit
+
+def dfs(graph, start_node):
+    visit = list()
+    stack = list()
+    stack.append(start_node)
+    while stack:
+        node = stack.pop()
+        if node not in visit:
+            visit.append(node)
+            stack.extend(graph[node])
+    return visit
+
+print(bfs(graph, 'A'))
+print(dfs(graph, 'A'))
