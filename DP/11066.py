a2ccaa90226054631502f7adcfa5728455ea995e
Choieeh <chyh0182@naver.com>
2020-12-20 14:27:29 +0000

    11066 solving

--- DP/11066.py
+++ DP/11066.py
@@ -1,17 +1,22 @@
 import sys
-
-T = int(sys.stdin.readline())
+input = sys.stdin.readline
+T = int(input())
 
 for t in range(T):
     count = 0
     temp = 0
-    N = sys.stdin.readline()
+    N = input()
     size = [int(x) for x in input().split()]
     size.sort()
     ss = size
-    while len(ss) == 1:
+    while len(ss) != 1:
         temp = ss[0] + ss[1]
         del ss[:2]
         count = count + temp
         ss.append(temp)
+        ss.sort()
     print(count)
+    
+'''
+경우의 수를 따지는 다른 접근이 필요할듯.
+'''
