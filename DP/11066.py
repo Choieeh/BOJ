c7469f5d5441aee83ba17f2e804850d51d3cac0a
Choieeh <chyh0182@naver.com>
2020-12-21 14:00:51 +0000

    11066 solving

--- DP/11066.py
+++ DP/11066.py
@@ -4,16 +4,14 @@ T = int(input())
 
 for t in range(T):
     count = 0
-    N = input()
+    N = int(input())
     size = [int(x) for x in input().split()]
-    dp = [[0 for i in range(N+1)]*for j in range(N+1)]
+    dp = [[0 for i in range(N+1)] for j in range(N+1)]
     
-    for i in range(1, N+1):
+    for i in range(1, N):
         dp[i][i+1] = size[i-1] + size[i]
-    
-    for i in range(1, N+1):
-        for j in range(1, N+1):
-            dp[i][j] = 
+        print(dp)
+
     
     
 '''
