6c08729ff794b9ba9d80a93bc47a5cc7f5ac1b2f
Choieeh <chyh0182@naver.com>
2020-12-23 14:44:15 +0000

    11049 solving

--- DP/11049.py
+++ DP/11049.py
@@ -2,8 +2,9 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-
-matrix = list(map(int, input().split()))
+matrix = [[0 for i in range(2)] for j in range(N)]
+for i in range(N):
+    matrix[i][0], matrix[i][1] = map(int, input().split())
 
 '''
 matrix
