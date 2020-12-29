7be4dfadb93bb3c560c04f55cb0ae261c0211d11
Choieeh <chyh0182@naver.com>
2020-12-29 14:28:15 +0000

    1260 solving with dictionary

---
+++ Graph/1260_2.py
@@ -0,0 +1,42 @@
+import sys
+input = sys.stdin.readline
+
+def bfs(start_node):
+    visit = list()
+    queue = list()
+    queue.append(start_node)
+    while queue:
+        node = queue.pop(0)
+        if node not in visit:
+            visit.append(node)
+            queue.extend(graph[node])
+            #extend는 list의 끝에 모든 항목을 덧붙여서 확장
+    return visit
+
+def dfs(start_node):
+    visit = list()
+    stack = list()
+    stack.append(start_node)
+    while stack:
+        node = stack.pop()
+        if node not in visit:
+            visit.append(node)
+            stack.extend(graph[node])
+    return visit
+
+N, M, V = map(int, input().split())
+
+graph={}
+visited = [0 for i in range(N+1)]
+
+for i in range(M):
+    x, y = map(int, input().split())
+    graph[x] = y
+    graph[y] = x
+
+print(graph)
+
+dfs(V)
+visited = [0 for i in range(N+1)]
+print()
+bfs(V)
