bc718336806d756f5c45ab1bc85addf048ea5016
Choieeh <chyh0182@naver.com>
2020-12-30 14:35:24 +0000

    2178 solving

---
+++ Graph/2178.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+matrix = [0 for i in range(N)]
+
+for i in range(N):
+    matrix[i] = input()
+    matrix[i] = list(matrix[i])
+    
+print(matrix)
