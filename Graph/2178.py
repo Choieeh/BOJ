f3c48d9b2a40831950f130beeb3072ed06fd1470
Choieeh <chyh0182@naver.com>
2021-01-03 03:54:50 +0000

    2178 clear bfs와 dfs 이용예시

--- Graph/2178.py
+++ Graph/2178.py
@@ -2,20 +2,23 @@ import sys
 input = sys.stdin.readline
 
 
-def dfs(x, y):
-    visited[x][y] = 1
-    count = count+1
-    if 
-    for i in range(1, N+1):
-        if visited[v][i] == 0 and matrix[v][i] == 1:
-            dfs()
-            
+def bfs(n, m):
+    queue = []
+    queue.append([n, m])
+    while queue:
+        a = queue.pop(0)
+        x = a[0]
+        y = a[1]
+        for dx, dy in (1,0), (-1,0),(0,1),(0,-1):
+            if 0 <= x+dx < N and 0 <= y+dy < M and matrix[x+dx][y+dy] ==1:
+                queue.append([x+dx, y+dy])
+                matrix[x+dx][y+dy] = matrix[x+dx][y+dy] + matrix[x][y]
+
             
 N, M = map(int, input().split())
 
 matrix = [0 for i in range(N)]
 visited = [[0 for i in range(M)] for j in range(N)]
-count = 0
 
 for i in range(N):
     matrix[i] = input()
@@ -23,4 +26,13 @@ for i in range(N):
     matrix[i] = list(map(int, matrix[i]))
     
 
+bfs(0, 0)
+print(matrix[N-1][M-1])
 print(matrix)
+
+
+'''
+dfs로 해결하려고 했으나 dfs로 하면 최단거리가 아닐수도 있음
+bfs사용해서 풀어야함.
+
+'''
