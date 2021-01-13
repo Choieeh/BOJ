8d819301399356f70c4a7285a8a20fda7f46afb8
Choieeh <chyh0182@naver.com>
2021-01-13 14:40:01 +0000

    7576 clear

--- Graph/7576.py
+++ Graph/7576.py
@@ -1,15 +1,15 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
 
-def bfs(x, y):
-    q = []
-    q.append([x, y])
+
+def bfs():
     while q:
-        a = q.pop(0)
+        a = q.popleft()
         x = a[0]
         y = a[1]
         for dx, dy in (1,0), (-1,0),(0,1),(0,-1):
-            if 0 <= x + dx < M and 0 <= y + dy < N and matrix[x+dx][y+dy] == 0:
+            if 0 <= x + dx < N and 0 <= y + dy < M and matrix[x+dx][y+dy] == 0:
                 matrix[x+dx][y+dy] = matrix[x][y] + 1
                 q.append([x+dx, y+dy])
     
@@ -17,22 +17,22 @@ def bfs(x, y):
 M, N = map(int,input().split())
 
 matrix = [0 for i in range(N)]
-
+q = deque()
 for i in range(N):
     matrix[i] = list(map(int, input().split()))
 
 for i in range(N):
     for j in range(M):
         if matrix[i][j] == 1:
-            q = bfs(i, j)
-
+            q.append([i, j])
+        
+bfs()
+result = max(map(max, matrix)) - 1
+            
 for i in range(N):
     for j in range(M):
         if matrix[i][j] == 0:
-            res = -1
+            result = -1
 
-            
-if res == -1:
-    result = -1
-elif res 
-print(matrix)
+
+print(result)
