8cd0f4e7075ebb3e144ce630d2e0e2e5e3ed1ba5
Choieeh <chyh0182@naver.com>
2021-02-17 13:26:56 +0000

    1065 clear

--- Brute/1065.py
+++ Brute/1065.py
@@ -1,3 +1,19 @@
 import sys
-input = sys.stdin.leadline
+input = sys.stdin.readline
 
+N = int(input())
+result = 0
+num = [0 for i in range(3)]
+
+if(N < 100):
+    result = N
+else:
+    result = 99
+    for i in range(100, N+1):
+        num[0] = int(i / 100)
+        num[1] = int((i % 100) / 10)
+        num[2] = int((i % 100) % 10)
+        if (num[0] - num[1]) == (num[1] - num[2]):
+            result = result + 1
+
+print(result)
