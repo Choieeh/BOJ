a29733ad1843c3a976eb6b783c3465d61ec61437
Choieeh <chyh0182@naver.com>
2021-02-19 13:31:49 +0000

    14891 solving

--- samsung/14891.py
+++ samsung/14891.py
@@ -2,14 +2,42 @@ import sys
 input = sys.stdin.readline
 num = 8
 
+def check_right(start, direction):
+    if start > 4 or meet[start-1] == 1:
+        return
+    
+    if meet[start-1] == -1:
+        check_right(start+1, -direction)
+        if direction == 1:
+            temp = tob.pop()
+            tob.insert(0, temp)
+        else:
+            temp = tob.pop(0)
+            tob.append(temp)
+
+def check_left(start, direction):
+    if start < 1 or meet[start] == 1:
+        return
+    
+    if meet[start] == -1:
+        check_left(start-1, -direction)
+        if direction == 1:
+            temp = tob.pop()
+            tob.insert(0, temp)
+        else:
+            temp = tob.pop(0)
+            tob.append(temp)
+
 tob = [0 for i in range(5)]
 meet = [0 for i in range(4)]
+result = 0
 
 for i in range(1, 5):
-    tob[i] = list(map(int, input().split()))
+    tempt = input()
+    tob[i] = list(map(int, str(tempt)))
 
 K = int(input())
-
+print(tob)
 turn = [0 for i in range(K)]
 for i in range(K):
     turn[i] = list(map(int, input().split()))
@@ -18,20 +46,18 @@ for i in range(K):
     t_num = turn[i][0]
     direction = turn[i][1]
     
-    for j in range(1, 4):
-        if tob[j][2] == tob[j+1][6]:
-            meet[j] = 1
-        else:
-            meet[j] = -1
-        
-    if t_num == 1:
-        for k in range(1, 4):
-            if meet[k] == 1:
-                
-    else if t_num == 2 or t_num == 3:
-        
+    check_right(t_num+1, direction)
+    check_left(t_num-1, direction)
+    
+    if direction == 1:
+        temp = tob.pop()
+        tob.insert(0, temp)
     else:
-        
+        temp = tob.pop(0)
+        tob.append(temp)
 
+result = tob[1][0]*1 + tob[2][0]*2 + tob[3][0]*4 + tob[4][0] * 8
+
+print(result)
 
 #1번 2 <-> 2번 6 2번 2 <-> 3번 6 3번 2 <-> 4번 6
