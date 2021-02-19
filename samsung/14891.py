41554ff9903f31ed09932f468f3a4d1500482c9d
Choieeh <chyh0182@naver.com>
2021-02-19 10:35:05 +0000

    14891 solving

--- samsung/14891.py
+++ samsung/14891.py
@@ -3,6 +3,7 @@ input = sys.stdin.readline
 num = 8
 
 tob = [0 for i in range(5)]
+meet = [0 for i in range(4)]
 
 for i in range(1, 5):
     tob[i] = list(map(int, input().split()))
@@ -15,8 +16,18 @@ for i in range(K):
 
 for i in range(K):
     t_num = turn[i][0]
-    if t_num == 1:
+    direction = turn[i][1]
+    
+    for j in range(1, 4):
+        if tob[j][2] == tob[j+1][6]:
+            meet[j] = 1
+        else:
+            meet[j] = -1
         
+    if t_num == 1:
+        for k in range(1, 4):
+            if meet[k] == 1:
+                
     else if t_num == 2 or t_num == 3:
         
     else:
