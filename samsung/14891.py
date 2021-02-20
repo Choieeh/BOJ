a784b76b0e4a9a3bdf025873446921d7c817c8b6
Choieeh <chyh0182@naver.com>
2021-02-20 14:11:16 +0000

    runtime error

--- samsung/14891.py
+++ samsung/14891.py
@@ -3,17 +3,17 @@ input = sys.stdin.readline
 num = 8
 
 def check_right(start, direction):
-    print(start)
+
     if start > 4 or meet[start-1] == 1:
         return
     
     if meet[start-1] == -1:
         check_right(start+1, -direction)
-        if direction == 1:
-            temp = tob.pop()
+        if direction == -1:
+            temp = tob[start].pop()
             tob[start].insert(0, temp)
         else:
-            temp = tob.pop(0)
+            temp = tob[start].pop(0)
             tob[start].append(temp)
 
 
@@ -23,7 +23,7 @@ def check_left(start, direction):
     
     if meet[start] == -1:
         check_left(start-1, -direction)
-        if direction == 1:
+        if direction == -1:
             temp = tob[start].pop()
             tob[start].insert(0, temp)
         else:
@@ -52,6 +52,7 @@ for i in range(K):
             meet[j] = 1
         else:
             meet[j] = -1
+
     check_right(t_num+1, direction)
     check_left(t_num-1, direction)
     
@@ -61,10 +62,8 @@ for i in range(K):
     else:
         temp = tob[t_num].pop(0)
         tob[t_num].append(temp)
-print(meet)
-print(tob)
+
 result = tob[1][0]*1 + tob[2][0]*2 + tob[3][0]*4 + tob[4][0] * 8
-print(tob[1][0], tob[2][0], tob[3][0], tob[4][0])
 print(result)
 
 #1번 2 <-> 2번 6 2번 2 <-> 3번 6 3번 2 <-> 4번 6
